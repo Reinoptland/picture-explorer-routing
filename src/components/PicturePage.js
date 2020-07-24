457fc319c9a1283f0d1afbcee098d04416bd61f8
Reinoptland <reinoptland@gmail.com>
2020-07-24 11:47:42 +0200

    Store pictures in state of picture page when it loads

--- src/components/PicturePage.js
+++ src/components/PicturePage.js
@@ -1,15 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function PicturePage() {
+  const [pictures, setPictures] = useState([]);
+
   useEffect(() => {
     // console.log("HI! useffeect??");
     async function fetchPictureList() {
       const response = await axios.get("https://picsum.photos/v2/list");
-      console.log("RESPONSE??", response);
+      console.log("RESPONSE??", response.data);
+      setPictures(response.data);
     }
 
     fetchPictureList();
   }, []);
+  console.log("PICS:", pictures); // who do we expect to see ..?
+  // - whatever we got from the api
+  // - Empty array first, then an array of objects (from the api) ?
+  //
   return <div>PICTURES HERE, TODO</div>;
 }
